af7f8f2c5ea5b85f54bdf8af434f1f2d7fac4ca9
Ethan Hill <ethan.hill1999@outlook.com>
2019-03-08 12:06:15 -0600

    IE 9

--- FountainPlus/Fountain.cs
+++ FountainPlus/Fountain.cs
@@ -12,7 +12,11 @@ namespace FountainPlus
         // Process input
         public static String Process(String input)
         {
-            return input.ToLower();
+            String output;
+            output = "<meta http-equiv=\"X - UA - Compatible\" content=\"IE = 9\" >\n";
+            output += input;
+            Console.WriteLine(output);
+            return output;
         }
     }
 }
