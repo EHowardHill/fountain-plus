df58dc9ff14a6ab1e51314906544b8420f45e82c
Ethan Hill <ethan.hill1999@outlook.com>
2019-03-22 16:32:43 -0500

    03 22 update

--- FountainPlus/MainWindow.xaml.cs
+++ FountainPlus/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Drawing;
 
 namespace FountainPlus
 {
@@ -8,11 +10,12 @@ namespace FountainPlus
 
         public MainWindow() {
             InitializeComponent();
+            Drop_Font.SelectedValue = "Consolas";
         }
 
         // Auto-Update
         private void BoxInput_TextChanged(object sender, TextChangedEventArgs e) {
-            if (Check_Update.IsChecked.GetValueOrDefault()) {
+            if (Check_AutoUpdate.IsChecked.GetValueOrDefault()) {
                 try { OutputBrowser.NavigateToString(Fountain.Process(boxInput.Text)); }
                 catch { OutputBrowser.NavigateToString("<html></html>"); }
             }
@@ -33,6 +36,52 @@ namespace FountainPlus
         {
             try { OutputBrowser.NavigateToString(Fountain.Process(boxInput.Text)); }
             catch { OutputBrowser.NavigateToString("<html></html>"); }
+        }
+
+        private void Drop_Interface_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (Drop_Interface.SelectedIndex)
+            {
+                case 0:     // Light
+                    try {
+                        Bkg_PreviewHub.Background = SystemColors.ControlLightBrush;
+                        Bkg_Tabbar.Background = SystemColors.ControlLightLightBrush;
+                        Label_Input.Foreground = SystemColors.ActiveCaptionTextBrush;
+                        Label_Outpt.Foreground = SystemColors.ActiveCaptionTextBrush;
+                        Label_Edt.Foreground = SystemColors.ActiveCaptionTextBrush;
+                        Label_Pre.Foreground = SystemColors.ActiveCaptionTextBrush;
+                        Label_Int.Foreground = SystemColors.ActiveCaptionTextBrush;
+                        boxInput.Background = SystemColors.ControlLightLightBrush;
+                        boxInput.Foreground = SystemColors.ActiveCaptionTextBrush;
+                    }
+                    catch { }
+                    break;
+                case 1:     // Dark
+                    try {
+                        Bkg_PreviewHub.Background = SystemColors.ActiveCaptionTextBrush;
+                        Bkg_Tabbar.Background = SystemColors.ControlDarkDarkBrush;
+                        Label_Input.Foreground = SystemColors.ControlLightLightBrush;
+                        Label_Outpt.Foreground = SystemColors.ControlLightLightBrush;
+                        Label_Edt.Foreground = SystemColors.ControlLightLightBrush;
+                        Label_Pre.Foreground = SystemColors.ControlLightLightBrush;
+                        Label_Int.Foreground = SystemColors.ControlLightLightBrush;
+                        boxInput.Background = SystemColors.ControlDarkDarkBrush;
+                        boxInput.Foreground = SystemColors.ControlLightLightBrush;
+                    }
+                    catch { }
+                    break;
+            }
+        }
+
+        private void Drop_Font_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                string font = Drop_Font.SelectedValue.ToString();
+                boxInput.FontFamily = new FontFamily(font);
+            }
+
+            catch { }
         }
     }
 }
